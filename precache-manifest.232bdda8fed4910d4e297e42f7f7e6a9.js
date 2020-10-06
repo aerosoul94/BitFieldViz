be8c2627d111e0b64e1dafe08146fce0b8d57b97
aerosoul <aerosoul94@hotmail.com>
2020-10-05 22:55:20 -0500

    Updates

---
+++ precache-manifest.232bdda8fed4910d4e297e42f7f7e6a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4507c96ebf900b70afbe70388f23faf8",
+    "url": "/BitFieldViz/index.html"
+  },
+  {
+    "revision": "64f00fe10095a5e18101",
+    "url": "/BitFieldViz/static/js/2.fe488fe7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BitFieldViz/static/js/2.fe488fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1aa88920371651a977",
+    "url": "/BitFieldViz/static/js/main.6e9ce51c.chunk.js"
+  },
+  {
+    "revision": "90fbf8441bea00e20666",
+    "url": "/BitFieldViz/static/js/runtime-main.e323055e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BitFieldViz/precache-manifest.232bdda8fed4910d4e297e42f7f7e6a9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BitFieldViz/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
